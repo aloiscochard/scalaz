3484e5fd45c1b8862c3d98c2a8e9e61ff547ddd3
Alois Cochard <alois.cochard@gmail.com>
2016-05-28 21:19:42 +0200

    Initial `base` module.

---
+++ base/src/main/scala/data/DisjunctionFunctions.scala
@@ -0,0 +1,8 @@
+package scalaz
+package data
+
+trait DisjunctionFunctions {
+  // TODO Can be inlined?
+  def left[L](value: L): Disjunction[L, Nothing] = Disjunction.L_(value)
+  def right[R](value: R): Disjunction[Nothing, R] = Disjunction.R_(value)
+}
---
+++ base/src/main/scala/data/DisjunctionInstances.scala
@@ -0,0 +1,27 @@
+package scalaz
+package data
+
+import clazz._
+import Disjunction.{\/, L_, R_}
+
+trait DisjunctionInstances {
+  implicit def monad[L]: Monad[L \/ ?] = new MonadClass[L \/ ?] {
+
+    override def map[A, B](ma: L \/ A)(f: A => B): L \/ B =
+      ma.fold[L \/ B](l => L_(l))(r => R_(f(r)))
+
+    override def ap[A, B](ma: L \/ A)(mf: L \/ (A => B)): L \/ B =
+      ma.fold[L \/ B](l => L_(l))(a => map[(A => B), B](mf)(f => f(a)))
+
+    override def pure[A](a: A): L \/ A =
+      R_[A](a)
+
+    override def flatMap[A, B](oa: L \/ A)(f: A => L \/ B): L \/ B =
+      oa.fold[L \/ B](l => L_(l))(a => f(a))
+  }
+
+  implicit def applicative[L]: Applicative[L \/ ?] = monad[L].applicative
+  implicit def apply[L]: Apply[L \/ ?] = monad[L].applicative.apply
+  implicit def functor[L]: Functor[L \/ ?] = monad[L].applicative.apply.functor
+  implicit def bind[L]: Bind[L \/ ?] = monad[L].bind
+}
