03af2a4a98af52d0dd0d2cec76b2750d663ee00c
Alois Cochard <alois.cochard@gmail.com>
2016-06-23 00:23:33 +0200

    Add `IList` todo in `Foldable`.

--- base/src/main/scala/typeclass/Foldable.scala
+++ base/src/main/scala/typeclass/Foldable.scala
@@ -4,11 +4,12 @@ package typeclass
 trait Foldable[F[_]] {
   def foldLeft[A, B](fa: F[A], z: B)(f: (B, A) => B): B
   def foldRight[A, B](fa: F[A], z: => B)(f: (A, => B) => B): B
+  // TODO Use IList (`toIList`)
   def toList[A](fa: F[A]): List[A] = foldLeft(fa, List[A]())((t, h) => h :: t).reverse
 }
 
 object Foldable extends FoldableInstances {
   def apply[F[_]](implicit F: Foldable[F]): Foldable[F] = F
 
-  object syntax extends FoldableSyntax
+  type Syntax = FoldableSyntax
 }
