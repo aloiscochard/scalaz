0642495f812dba7730d4eca283044a027533e634
Alois Cochard <alois.cochard@gmail.com>
2016-07-31 18:05:38 +0200

    WIP

--- base/src/main/scala/data/Optic.scala
+++ base/src/main/scala/data/Optic.scala
@@ -2,4 +2,12 @@ package scalaz
 package data
 
 object Optic extends OpticTypes
-              with PrismFunctions
+              with PrismFunctions {
+
+  case class Scope[Optics <: meta.HList](optics: Optics)
+
+  object Scope {
+    import scala.language.experimental.macros
+    def run[Optics <: meta.HList, A](optics: Optics): A = macro meta.Optics.run[Optics, A]
+  }
+}
---
+++ meta/src/main/scala/HList.scala
@@ -0,0 +1,9 @@
+package scalaz
+package meta
+
+sealed trait HList
+
+object HList {
+  final case class HCons[H, T <: HList](head: H, tail: T) extends HList
+  case object HNil extends HList
+}
