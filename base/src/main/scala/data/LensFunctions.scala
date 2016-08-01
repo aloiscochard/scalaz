c16c047996e401051d1d734beb3b47f5d73d8589
Alois Cochard <alois.cochard@gmail.com>
2016-08-02 00:32:14 +0200

    WIP

---
+++ base/src/main/scala/data/LensFunctions.scala
@@ -0,0 +1,12 @@
+package scalaz
+package data
+
+import typeclass.Functor
+
+trait LensFunctions {
+  def lens[S, T, A, B](sa: S => A)(sbt: S => B => T): Lens[S, T, A, B] = new Lens[S, T, A, B] {
+    def get(s: S): A = sa(s)
+    def set(s: S, b: B): T = sbt(s)(b)
+  }
+  def slens[S, A](sa: S => A)(sas: S => A => S): Lens[S, S, A, A] = lens[S, S, A, A](sa)(sas)
+}
